55c1c0b4e60e2618d9df8852245bfc4e75871f62
mariska OLWAGEN <molwage@c2r4s6.wethinkcode.co.za>
2019-01-15 13:34:08 +0200

    cleanup

--- login/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //connect to the database
-    $db = mysqli_connect('localhost', 'root', '', 'registration');
-    //if register button is pressed
-    if(isset($_POST['register']))
-    {
-        $username = mysql_real_escape_string($_POST['username'])
-    }
-?>
